27147a48d24766eef1e4be802a8d709ce2ebea56
ZSHenChan <zishenchan@gmail.com>
2026-03-03 11:12:22 +0800

    config(server): update server env

--- app/env/server.ts
+++ app/env/server.ts
@@ -4,7 +4,6 @@ import { z } from "zod";
 export const envServer = createEnv({
   emptyStringAsUndefined: true,
   server: {
-    GOOGLE_CONSOLE_API_KEY: z.string(),
     GEMINI_API_KEY: z.string(),
     TXTAI_BASE_URL: z.string(),
     REMINDER_API_TOKEN: z.string(),
@@ -14,7 +13,6 @@ export const envServer = createEnv({
     AWS_BUCKET_NAME: z.string(),
   },
   experimental__runtimeEnv: {
-    GOOGLE_CONSOLE_API_KEY: process.env.GOOGLE_CONSOLE_API_KEY,
     GEMINI_API_KEY: process.env.GEMINI_API_KEY,
     TXTAI_BASE_URL: process.env.TXTAI_BASE_URL,
     AWS_REGION: process.env.AWS_REGION,
